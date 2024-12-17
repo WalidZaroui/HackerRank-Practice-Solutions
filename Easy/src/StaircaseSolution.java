336122d91e038a4889e47ef0534d7f71785e840b
zarwi.walid <zarwi.walid@gmail.com>
2024-12-17 17:54:13 +0100

    Staircase

---
+++ Easy/src/StaircaseSolution.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+class ResultStaircase {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+        // Write your code here
+        for(int i = 0 ;i<n;i++){
+
+            for(int j = 0 ;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j = n-i-1 ;j<n;j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+public class StaircaseSolution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        ResultStaircase.staircase(n);
+
+        bufferedReader.close();
+    }
+}
