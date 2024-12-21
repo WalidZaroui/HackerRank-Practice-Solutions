1fbb3957ed58598fbaa97052f3b823f82f7a8265
zarwi.walid <zarwi.walid@gmail.com>
2024-12-21 09:23:04 +0100

    NumberLineJumps

---
+++ Easy/src/Implementation/NumberLineJumps.java
@@ -0,0 +1,56 @@
+package Implementation;
+import java.io.*;
+class ResultNumberLineJumps {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Write your code here
+        while(x1<=x2 && v1>=v2 || x1>=x2 && v1<=v2 ){
+            if(x1==x2){
+                System.out.println("YES");
+                return "YES";
+            }else{
+                x1+=v1;
+                x2+=v2;
+            }
+        }
+        System.out.println("NO");
+        return "NO";
+    }
+
+}
+
+public class NumberLineJumps {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = ResultNumberLineJumps.kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
